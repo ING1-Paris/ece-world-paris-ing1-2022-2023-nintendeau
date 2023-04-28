1eb8aa8a12dc4d59c9d826541f0bd30b40fc45a7
Alfred de Vulpian <derfladv@gmail.com>
2023-04-28 15:07:51 +0200

    GUITAR HERO : gestion des notes par liste chainée, les notes défilent de haut en bas en fonction du nombre de frames, boutons désormais fonctionnels, gestion du score et de la vie du joueur

--- attractions/guiter_hero.c
+++ attractions/guiter_hero.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 #define MAX_INTERVALLE 70
-#define MIN_INTERVALLE 30
+#define MIN_INTERVALLE 35
 #define NB_CORDES 4
 
 
@@ -74,19 +74,22 @@ int main() {
     BITMAP * buffer = create_bitmap(SCREEN_W, SCREEN_H);
     BITMAP * stage = create_bitmap(SCREEN_W/3, SCREEN_H);
     BITMAP * final_screen = create_bitmap(SCREEN_W, SCREEN_H);
-    BITMAP * logo = load_bitmap("../assets/guitar_hero/logo.bmp", NULL);
+    BITMAP * logo = load_bitmap("../assets/logo.bmp", NULL);
 
     int positions[NB_CORDES] = {stage->w/5, stage->w*2/5, stage->w*3/5, stage->w*4/5, };
     int couleurs[NB_CORDES] = {makecol(103, 147, 52), makecol(195, 64, 60), makecol(228, 207, 76), makecol(90, 157, 188)};
 
     if (!logo) {
-        logo = load_bitmap("assets\\guitar_hero\\logo.bmp", NULL);
+        logo = load_bitmap("assets\\logo/bmp", NULL);
         if (!logo) {
             allegro_message("IMAGE ERROR");
             allegro_exit();
             exit(EXIT_FAILURE);
         }
     }
+
+
+    Note * ancre = NULL;
 
     while (!key[KEY_ESC]) {
 
@@ -99,11 +102,72 @@ int main() {
             rectfill(stage, positions[i], 0, positions[i] + 1, SCREEN_H, couleurs[i]);
         }
 
+        show_mouse(screen);
+
+        Note * nouvelle_note = malloc(sizeof(Note));
+
+        if (frame_counter % intervalle == 0) {
+            intervalle--;
+            if (intervalle < MIN_INTERVALLE) {
+                intervalle = MIN_INTERVALLE;
+            }
+
+            int random_number = rand() % NB_CORDES;
+            nouvelle_note->x = positions[random_number];
+            nouvelle_note->y = 0;
+            nouvelle_note->y_speed = 7;
+            nouvelle_note->color = couleurs[random_number];
+            nouvelle_note->alive = 1;
+            nouvelle_note->next = NULL;
+
+            if (*&ancre == NULL) {
+                *&ancre = nouvelle_note;
+            }
+            else {
+                nouvelle_note->next = *&ancre;
+                *&ancre =nouvelle_note;
+            }
+        }
+
+
+        Note * notes = ancre;
+        Note * current = notes;
+
+        while (current->next != NULL) {
+            if (current->alive == 1) {
+                printf("x : %d, y : %d\n", current->x, current->y);
+                circlefill(stage, current->x, current->y, 20, current->color);
+                if (current->y <= SCREEN_H - 75 && current->y >= SCREEN_H - 100) {
+                    if (key[KEY_D] && current->x == positions[0] || key[KEY_F] && current->x == positions[1] || key[KEY_J] && current->x == positions[2] || key[KEY_K] && current->x == positions[3]) {
+                        current->alive = 0;
+                        player->score += 10;
+                    }
+                }
+                else if (current->y > SCREEN_H - 10){
+                    current->alive = 0;
+                    player->life -= 1;
+                }
+            }
+            current = current->next;
+        }
+
+        current = notes;
+
+        while (current->next != NULL) {
+            if (current->alive == 1) {
+                current->y += current->y_speed;
+            }
+            current = current->next;
+        }
+
+
+        frame_counter++;
 
         afficher_boutons(stage, positions, couleurs);
         blit(stage, buffer, 0, 0, SCREEN_W/3, 0, SCREEN_W, SCREEN_H);
         rectfill(buffer, 0, 0, SCREEN_W/3 - 1, 200, makecol(255, 0, 0));
         affichage_score(player, buffer);
+
 
         blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
 
