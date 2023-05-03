c7fbfa47da65798f49df54c6074a7aa857241a00
Léon <dalleleon@gmail.com>
2023-05-03 11:37:06 +0200

    Petite Update snake : Code plus propre, collisions mieux gérées (avec les instructions de Mr Fercquot) A venir : Mouvement du reste du corps

--- attractions/snake.c
+++ attractions/snake.c
@@ -26,42 +26,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <allegro.h>
+#include <stdbool.h>
 
-// Constantes pour la taille des blocs et de l'écran
-#define BLOCK_SIZE 20
+//* Constantes pour la taille des blocs et de l'écran
+#define BLOCK_SIZE 10
+#define SPEED 1
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-// Structure pour les blocs du serpent
+//* Structures pour les blocs du serpent et la nourriture
 typedef struct snake {
     int x, y;
     int direction;
+    int previous_pos_x, previous_pos_y;
     struct snake *next;
+    bool is_head;
 } snake;
-
-// Structure pour la nourriture
 typedef struct food {
-    int x, y;
+    int x;
+    int y;
 } food;
 
-//Initialisation des serpents
-void init_snake(snake *head, int x, int y) {
-    head->x = x;
-    head->y = y;
-    head->direction = rand() % 4; //direction aléatoire
-    head->next = NULL;
-} 
-
-//Affichage du serpent
-void draw_snake(snake *head, int color) {
-    snake *current_block = head;
-    while (current_block != NULL) {
-        rectfill(screen, current_block->x, current_block->y, current_block->x + BLOCK_SIZE, current_block->y + BLOCK_SIZE, color);
-        current_block = current_block->next;
-    }
-}
-
-int main(int argc, char *argv[]){
+//*Fonctions d'initialisation
+void init_allegro(){
     //Initialisation de la seed pour les nombres aléatoires
     srand(time(NULL));
     //Initialisation d'Allegro
@@ -70,46 +57,79 @@ int main(int argc, char *argv[]){
     set_color_depth(32);
     set_window_title("Snake");
     set_color_depth(desktop_color_depth());
-    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1200, 800, 0, 0) != 0) {
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0) != 0) {
         allegro_message("GFX ERROR");
         allegro_exit();
         exit(EXIT_FAILURE);
     }
+}
+    //¤Initialisation des serpents (direction aléatoire)
+void init_snake(snake *head, int x, int y) {
+    head->x = x;
+    head->y = y;
+    head->direction = rand() % 4; //direction aléatoire
+    head->previous_pos_x = x;
+    head->previous_pos_y = y;
+    head->is_head = true;
+    head->next = NULL;
+} 
+    //¤Initialisation de la nourriture (position aléatoire)
+void init_food(food *food) {
+    food->x = rand() % (SCREEN_WIDTH - BLOCK_SIZE);
+    food->y = rand() % (SCREEN_HEIGHT - BLOCK_SIZE);
+}
+    //¤Affichage du serpent à partir de la tête
+void draw_snake(snake *head, int color) {
+    snake *current_block = head;
+    while (current_block != NULL) {
+        rectfill(screen, current_block->x, current_block->y, current_block->x + BLOCK_SIZE, current_block->y + BLOCK_SIZE, color);
+        current_block = current_block->next;
+    }
+}
+    //¤Fonction pour libérer la mémoire
+void lib_memoire(snake *head, food *food) {
+    snake *current_block = head;
+    snake *next_block = head->next;
+    while (current_block != NULL) {
+        next_block = current_block->next;
+        free(current_block);
+        current_block = next_block;
+    }
+    free(food);
+}
 
-    //Initialisation des variables
+//!Fonctions relatives au serpent
+
+
+
+
+
+
+int main(int argc, char *argv[]){
+    init_allegro();
+    //?Initialisation des variables 
     int game_over = 0;
     int score1 = 0;
+    int score2 = 0;
     int color1 = makecol(255, 0, 0);
 
-    //Initialisation du serpent 1 (ses caractéristiques sont dans la fonction init_snake : position, direction, prochain bloc)
-    snake *head1;
-    head1 = malloc(sizeof(snake));
-    init_snake(head1, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);  
-
-    //Initialisation de la nourriture et de sa position (seule caractéristique)
-    food *food1;
-    food1 = malloc(sizeof(food));
-    food1->x = rand() % (SCREEN_WIDTH / BLOCK_SIZE) * BLOCK_SIZE;
-    food1->y = rand() % (SCREEN_HEIGHT / BLOCK_SIZE) * BLOCK_SIZE;
+    //?Initialisation du serpent 1 (ses caractéristiques sont dans la fonction init_snake : position, direction, prochain bloc et si c'est la tête) et la nourriture et de sa position (seule caractéristique)
+    snake *head1 = malloc(sizeof(snake));
+    init_snake(head1, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    food *food1 = malloc(sizeof(food));
+    init_food(food1);
 
     //Boucle principale
-    while(key != KEY_ESC && !game_over){
-        //Effacement de l'écran (actualisation)
+    while(!key[KEY_ESC] && !game_over){
+        //Effacement de l'écran (actualisation) et de affichage de la nourriture et du score
         clear_to_color(screen, makecol(0, 0, 0));
-
-        //Affichage de la nourriture
+        textprintf_ex(screen, font, 10, 10, makecol(255, 255, 255), -1, "Score : %d", score1);
         rectfill(screen, food1->x, food1->y, food1->x + BLOCK_SIZE, food1->y + BLOCK_SIZE, makecol(0, 255, 0));
-
-        //Affichage du serpent 1
         draw_snake(head1, color1);
 
-        //Affichage du score
-        textprintf_ex(screen, font, 10, 10, makecol(255, 255, 255), -1, "Score : %d", score1);
+        
 
-        //Actualisation de l'écran
-        vsync();
-
-        //Déplacement du serpent 1
+        //Déplacement du serpent 1 en fonction de la direction (0 = haut, 1 = droite, 2 = bas, 3 = gauche, pas de demi-tour possible)
         if (key[KEY_UP] && head1->direction != 2) {
             head1->direction = 0;
         }
@@ -137,31 +157,27 @@ int main(int argc, char *argv[]){
             head1->x -= BLOCK_SIZE;
         }
 
+
         //Gestion des collisions avec les bords de l'écran
         if (head1->x < 0 || head1->x >= SCREEN_WIDTH || head1->y < 0 || head1->y >= SCREEN_HEIGHT) {
+            printf("Game Over ! Out of bounds !\n");
             game_over = 1;
         }
-
-        //Gestion des collisions avec la nourriture
-        if (head1->x == food1->x && head1->y == food1->y) {
+        
+        //Gestion des collisions avec la nourriture en fonction de la taille d'un bloc
+        /// (2 pts) Si la tête du serpent 1 est dans la zone de la nourriture, le score augmente de 1, un nouveau bloc est créé et la nourriture change de position
+        if (head1->x <= food1->x + BLOCK_SIZE && food1->x <= head1->x + BLOCK_SIZE && head1->y <= food1->y + BLOCK_SIZE && food1->y <= head1->y + BLOCK_SIZE) {
             score1++;
-            food1->x = rand() % (SCREEN_WIDTH / BLOCK_SIZE) * BLOCK_SIZE;
-            food1->y = rand() % (SCREEN_HEIGHT / BLOCK_SIZE) * BLOCK_SIZE;
+            init_food(food1);
         }
 
-        //Gestion des collisions avec le serpent 1
-        snake *current_block = head1->next;
-        while (current_block != NULL) {
-            if (head1->x == current_block->x && head1->y == current_block->y) {
-                game_over = 1;
-            }
-            current_block = current_block->next;
-        }
-
-        //C'est bon
+        //Actualisation de l'écran
         rest(100);
     }
-    //Fermeture d'Allegro
+
+
+    //¤Fin du programme
+    lib_memoire(head1, food1);
     allegro_exit();
     return 0;
 }END_OF_MAIN()
