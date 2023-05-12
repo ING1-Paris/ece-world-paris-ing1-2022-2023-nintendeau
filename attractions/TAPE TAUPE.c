e85c11650ccd9ea58c5ea5c5d2089402ff95ed8b
MatoxRyu <114871597+MatoxRyu@users.noreply.github.com>
2023-05-12 11:41:59 +0200

    Update TAPE TAUPE.c
    - simplification du code 
    - création d'une structure pour le nombre de point par joueur
    - affichage des scores

--- attractions/TAPE TAUPE.c
+++ attractions/TAPE TAUPE.c
@@ -2,6 +2,28 @@
 #include <time.h>
 #include <allegro.h>
 
+
+typedef struct {
+    int scoreJ1;
+    int scoreJ2;
+} ScoreJoueurs;
+
+void dessinerRectangleBleu(int x, int y, int cbleu, int crouge, ScoreJoueurs * scores, int compteur, int joueuractif) {
+    for (int i = 0; i < compteur; i++) {
+        rectfill(screen, x, y, x + 100, y + 100, cbleu);
+        if (mouse_b) {
+            if (mouse_x >= x && mouse_x <= x + 100 && mouse_y >= y && mouse_y <= y + 100) {
+                printf("bouton de la souris enfoncé\n");
+                if (joueuractif == 1) scores->scoreJ1++;
+                else if (joueuractif == 2) scores->scoreJ2++;
+                rectfill(screen, x, y, x + 100, y + 100, crouge);
+                rest(500);
+                compteur = 0;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
@@ -22,8 +44,19 @@ int main(int argc, char *argv[]) {
     int scoreJ1 = 0, scoreJ2 = 0, nbrTicket = 5;
     int tempsBleu = 0;
 
+    ScoreJoueurs * scores;
+    scores->scoreJ1 = 0;
+    scores->scoreJ2 = 0;
+
+    int joueuractif = 1;
+
     while(!fin){
-        textprintf_ex(screen, font, 10, 10, cblanc, -1, "Joueur 1 : %d", scoreJ1);
+
+        rectfill(screen, 8, 10, 110, 20, cbleu);
+        textprintf_ex(screen, font, 10, 10, cblanc, -1, "Joueur 1 : %d", scores->scoreJ1);
+        rectfill(screen, 8, 20, 110, 30, crouge);
+        textprintf_ex(screen, font, 10, 20, cblanc, -1, "Joueur 2 : %d", scores->scoreJ2);
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
                 rectfill(screen, 130 + i * 150, 100 + j * 150, 130 + i * 150 + 100, 100 + j * 150 + 100, cvert);
@@ -32,176 +65,43 @@ int main(int argc, char *argv[]) {
         int rectbleu = rand() % 12 + 1;
         int tempsderepos = rand() % 500 + 500;
         int compteur = rand () % 6000 + 4000;
-
         rest (tempsderepos);
         if (rectbleu == 1) {
             printf("rectbleu vaut 1\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 130, 100, 130 + 100, 100 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 130 && mouse_x <= 230 && mouse_y >= 100 && mouse_y <= 200) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 130, 100, 130 + 100, 100 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(130, 100, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 2) {
             printf("rectbleu vaut 2\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 280, 100, 280 + 100, 100 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 280 && mouse_x <= 380 && mouse_y >= 100 && mouse_y <= 200) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 280, 100, 280 + 100, 100 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(280, 100, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 3) {
             printf("rectbleu vaut 3\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 430, 100, 430 + 100, 100 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 430 && mouse_x <= 530 && mouse_y >= 100 && mouse_y <= 200) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 430, 100, 430 + 100, 100 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(430, 100, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 4) {
             printf("rectbleu vaut 4\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 580, 100, 580 + 100, 100 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 580 && mouse_x <= 680 && mouse_y >= 100 && mouse_y <= 200) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 580, 100, 580 + 100, 100 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(580, 100, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 5) {
             printf("rectbleu vaut 5\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 130, 250, 130 + 100, 250 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 130 && mouse_x <= 230 && mouse_y >= 250 && mouse_y <= 350) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 130, 250, 130 + 100, 250 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(130, 250, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 6) {
             printf("rectbleu vaut 6\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 280, 250, 280 + 100, 250 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 280 && mouse_x <= 380 && mouse_y >= 250 && mouse_y <= 350) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 280, 250, 280 + 100, 250 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(280, 250, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 7) {
             printf("rectbleu vaut 7\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 430, 250, 430 + 100, 250 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 430 && mouse_x <= 530 && mouse_y >= 250 && mouse_y <= 350) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 430, 250, 430 + 100, 250 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(430, 250, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 8) {
             printf("rectbleu vaut 8\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 580, 250, 580 + 100, 250 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 580 && mouse_x <= 680 && mouse_y >= 250 && mouse_y <= 350) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 580, 250, 580 + 100, 250 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(580, 250, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 9) {
             printf("rectbleu vaut 9\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 130, 400, 130 + 100, 400 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 130 && mouse_x <= 230 && mouse_y >= 400 && mouse_y <= 500) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 130, 400, 130 + 100, 400 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(130, 400, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 10) {
             printf("rectbleu vaut 10\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 280, 400, 280 + 100, 400 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 280 && mouse_x <= 380 && mouse_y >= 400 && mouse_y <= 500) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 280, 400, 280 + 100, 400 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(280, 400, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 11) {
             printf("rectbleu vaut 11\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 430, 400, 430 + 100, 400 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 430 && mouse_x <= 530 && mouse_y >= 400 && mouse_y <= 500) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 430, 400, 430 + 100, 400 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(430, 400, cbleu, crouge, scores, compteur, joueuractif);
         }else if (rectbleu == 12) {
             printf("rectbleu vaut 12\n");
-            for (int i = 0; i < compteur; i++){
-                rectfill(screen, 580, 400, 580 + 100, 400 + 100, cbleu);
-                if (mouse_b) {
-                    if (mouse_x >= 580 && mouse_x <= 680 && mouse_y >= 400 && mouse_y <= 500) {
-                        printf("bouton de la souris enfoncé\n");
-                        scoreJ1++;
-                        rectfill(screen, 580, 400, 580 + 100, 400 + 100, crouge);
-                        rest(500);
-                        compteur = 0;
-                    }
-                }
-            }
+            dessinerRectangleBleu(580, 400, cbleu, crouge, scores, compteur, joueuractif);
         }
     }
 }
