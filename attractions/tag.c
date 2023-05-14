6ff46da26159cf508c11656cd4e11ec2492b1104
Léon <dalleleon@gmail.com>
2023-05-15 00:38:37 +0200

    🚀 RELEASE:

--- attractions/tag.c
+++ attractions/tag.c
@@ -28,6 +28,8 @@ typedef struct {
     BITMAP *sprite;     // Sprite du bonhomme
     int x, y;           // Coordonnées du bonhomme
     double xspeed, yspeed; // Vitesse horizontale et verticale du bonhomme
+    int previous_x;   // Position X précédente du bonhomme
+    int previous_y;   // Position Y précédente du bonhomme
 } Bonhomme;
 
 void init_allegro(){ //¤Initialisation des serpents (direction aléatoire)
@@ -45,42 +47,103 @@ void init_allegro(){ //¤Initialisation des serpents (direction aléatoire)
         exit(EXIT_FAILURE);
     }
 }
-void mouvement(Bonhomme *bonhomme, int groundLevel, int HORIZONTAL_SPEED, int MAX_JUMP_HEIGHT, double GRAVITY, double MAX_FALL_SPEED, int UP, int RIGHT, int LEFT){
+
+void mouvement(Bonhomme *bonhomme, BITMAP *calque_collision, int groundLevel, int HORIZONTAL_SPEED, int MAX_JUMP_HEIGHT, double GRAVITY, double MAX_FALL_SPEED, int UP, int RIGHT, int LEFT) {
+    // Update bonhomme's position
+    bonhomme->previous_x = bonhomme->x;
+    bonhomme->previous_y = bonhomme->y;
+
     // Update bonhomme's position
     bonhomme->x += bonhomme->xspeed;
     bonhomme->y += (int)bonhomme->yspeed;
 
+    //colors of each corners of the bonhomme
+    int color_down_left = getpixel(calque_collision, (bonhomme->x)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h/1.3)*calque_collision->h/SCREEN_H + 7);
+    int color_down_right = getpixel(calque_collision, (bonhomme->x + bonhomme->sprite->w)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h/1.3)*calque_collision->h/SCREEN_H + 7);
+    int color_up_left = getpixel(calque_collision, (bonhomme->x)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h)*calque_collision->h/SCREEN_H - 25);  
+    int color_up_right = getpixel(calque_collision, (bonhomme->x + bonhomme->sprite->w)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h)*calque_collision->h/SCREEN_H - 25);
+    int color_mid_right = getpixel(calque_collision, (bonhomme->x + bonhomme->sprite->w)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h/1.3)*calque_collision->h/SCREEN_H);
+    int color_mid_left = getpixel(calque_collision, (bonhomme->x)*calque_collision->w/SCREEN_W, (bonhomme->y + bonhomme->sprite->h/1.3)*calque_collision->h/SCREEN_H );
+    int wall = makecol(255, 0, 0);
+    int isOnGround = (color_down_left == wall || color_down_right == wall) ? TRUE : FALSE;
+    int isHeadOnCeiling = (color_up_left == wall || color_up_right == wall) ? TRUE : FALSE;
+
     // Handle keyboard input
     if (key[RIGHT]) {
         // Move right
-        bonhomme->xspeed = HORIZONTAL_SPEED;
+        if (bonhomme->xspeed >= 0 && color_mid_right != wall) {
+            bonhomme->xspeed = HORIZONTAL_SPEED;
+        } else {
+            bonhomme->xspeed = 0;
+        }
     } else if (key[LEFT]) {
         // Move left
-        bonhomme->xspeed = -HORIZONTAL_SPEED;
+        if (bonhomme->xspeed <= 0 && color_mid_left != wall) {
+            bonhomme->xspeed = -HORIZONTAL_SPEED;
+        } else {
+            bonhomme->xspeed = 0;
+        }
     } else {
         // Stop horizontal movement
         bonhomme->xspeed = 0;
     }
 
-    // Jump only if the bonhomme is on the ground
-    if (key[UP] && bonhomme->y == groundLevel) {
+    // Prevent bonhomme from falling through the ground
+    if ((color_down_left == wall && color_up_left != wall)|| (color_down_right == wall && color_up_right != wall)) {
+        bonhomme->y = bonhomme->previous_y;
+        bonhomme->yspeed = 0;
+        printf("On est collé au mur\n");
+
+    }
+
+    // Prevent Bonhomme from going head first into a platform
+    if ((color_up_left == wall || color_up_right == wall)) {
+        bonhomme->y = bonhomme->previous_y;
+        bonhomme->yspeed = 0;
+    }
+
+    // Prevent Bonhomme from going through a wall horizontally
+    if (((color_down_left == wall && color_mid_left == wall && color_up_left == wall ) || (color_down_right == wall && color_mid_right == wall && color_up_right == wall))) {
+        bonhomme->x = bonhomme->previous_x;
+        
+    }
+
+
+    // Jump only if the bonhomme is on the ground or on a platform
+    if (key[UP] && isOnGround) {
         bonhomme->yspeed = -MAX_JUMP_HEIGHT; // Set the initial vertical speed for the jump
+    }
+
+    //when the middle of the bonhomme is on a platform, he goes back to previous position
+    if (color_mid_left == wall && color_mid_right == wall) {
+        bonhomme->x = bonhomme->previous_x;
     }
 
     // Apply gravity
     bonhomme->yspeed += GRAVITY;
 
-    // Limit the maximum falling speed
-    if (bonhomme->yspeed >= MAX_FALL_SPEED) {
+    // Limit the falling speed
+    if (bonhomme->yspeed > MAX_FALL_SPEED) {
         bonhomme->yspeed = MAX_FALL_SPEED;
     }
 
-    // Limit the maximum height of the jump
-    if (bonhomme->y > groundLevel) {
-        bonhomme->y = groundLevel;
+    // Limit the movements to the screen's size
+    if (bonhomme->x < 0) {
+        bonhomme->x = 0;
+        bonhomme->xspeed = 0;
+    } else if (bonhomme->x > SCREEN_W - bonhomme->sprite->w) {
+        bonhomme->x = SCREEN_W - bonhomme->sprite->w;
+        bonhomme->xspeed = 0;
+    } else if (bonhomme->y < 0) {
+        bonhomme->y = 0;
+        bonhomme->yspeed = 0;
+    } else if (bonhomme->y > SCREEN_H - bonhomme->sprite->h) {
+        bonhomme->y = SCREEN_H - bonhomme->sprite->h;
         bonhomme->yspeed = 0;
     }
 }
+
+
 BITMAP* image_loader(const char* filepath){
     // on vérifie que les BITMAPS ont bien été initialisés
     char clion_filepath[100];
@@ -104,31 +167,35 @@ int main()
     init_allegro();
 
     //! Initialisation des variables du mouvement
-    double GRAVITY = 0.5;
+    double GRAVITY = 0.3;
     double MAX_FALL_SPEED = 12;
-    int HORIZONTAL_SPEED = 7;
-    int MAX_JUMP_HEIGHT = 12;
-    int groundLevel = SCREEN_HEIGHT - 100;
+    int HORIZONTAL_SPEED = 6;
+    int MAX_JUMP_HEIGHT = 14;
+    int groundLevel = SCREEN_HEIGHT - 110;
     //!##########################################
 
     // Création du buffer
     BITMAP *buffer = create_bitmap(SCREEN_WIDTH, SCREEN_HEIGHT);
+    BITMAP * map = image_loader("assets/tag/map.bmp");
+    BITMAP * calque_collisions = image_loader("assets/tag/map_collisions.bmp");
 
     // Création du bonhomme
-    Bonhomme bonhomme1 = {image_loader("assets/palais_des_glaces/player_1.bmp"), SCREEN_WIDTH/2, groundLevel, 0, 0};
-    Bonhomme bonhomme2 = {image_loader("assets/palais_des_glaces/player_2.bmp"), SCREEN_WIDTH/3, groundLevel, 0, 0};
+    Bonhomme bonhomme1 = {image_loader("assets/tag/player_1.bmp"), SCREEN_WIDTH/2, groundLevel, 0, 0};
+    Bonhomme bonhomme2 = {image_loader("assets/tag/player_2.bmp"), SCREEN_WIDTH/3, groundLevel, 0, 0};
 
     // Boucle principale
     while (!key[KEY_ESC]) {
         
         // Update bonhomme's position
-        mouvement(&bonhomme1, groundLevel, HORIZONTAL_SPEED, MAX_JUMP_HEIGHT, GRAVITY, MAX_FALL_SPEED, KEY_UP, KEY_RIGHT, KEY_LEFT);
-        mouvement(&bonhomme2, groundLevel, HORIZONTAL_SPEED, MAX_JUMP_HEIGHT, GRAVITY, MAX_FALL_SPEED, KEY_W, KEY_D, KEY_A);
+        mouvement(&bonhomme1, calque_collisions, groundLevel, HORIZONTAL_SPEED, MAX_JUMP_HEIGHT, GRAVITY, MAX_FALL_SPEED, KEY_UP, KEY_RIGHT, KEY_LEFT);
+        mouvement(&bonhomme2, calque_collisions, groundLevel, HORIZONTAL_SPEED, MAX_JUMP_HEIGHT, GRAVITY, MAX_FALL_SPEED, KEY_W, KEY_D, KEY_A);
 
         // Effacer l'écran précédent
         clear(buffer);
 
         // Draw bonhomme sprite at its updated position
+        stretch_blit(calque_collisions, buffer, 0, 0, calque_collisions->w, calque_collisions->h, 0, 0, buffer->w, buffer->h);
+        //stretch_blit(map, buffer, 0, 0, map->w, map->h, 0, 0, buffer->w, buffer->h);
         draw_sprite(buffer, bonhomme1.sprite, bonhomme1.x, bonhomme1.y);
         draw_sprite(buffer, bonhomme2.sprite, bonhomme2.x, bonhomme2.y);
 
