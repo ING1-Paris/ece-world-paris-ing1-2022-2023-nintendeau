d4889f6d0c514233f91604671df3091fbeba5ab7
MatoxRyu <114871597+MatoxRyu@users.noreply.github.com>
2023-05-15 16:06:35 +0200

    Update TAPE TAUPE.c
    - début de l'ajout des sons et bruitages 
    - réglage de la valeur des scores
    - affichage opérationnel

--- attractions/TAPE TAUPE.c
+++ attractions/TAPE TAUPE.c
@@ -3,60 +3,66 @@
 #include <allegro.h>
 
 
-void dessinerRectangleBleu(int x, int y, int cbleu, int crouge, int compteur, int joueuractif, int* J1, int* J2, BITMAP* dessprite2, BITMAP* dessprite3){
+void dessinerRectangleBleu(int x, int y, int cbleu, int crouge, int compteur, int joueuractif, int* J1, int* J2, BITMAP* dessprite2, BITMAP* dessprite3, BITMAP* dessprite4){
     for (int i = 0; i < compteur; i++) {
         draw_sprite(screen, dessprite2, x, y);
         if (mouse_b) {
             if (mouse_x >= x && mouse_x <= x + 100 && mouse_y >= y && mouse_y <= y + 100) {
                 draw_sprite(screen, dessprite3,x,y); //taupe touchée
                 rest(500);
+                draw_sprite(screen, dessprite4,x,y); //taupe morte
+                rest(500);
                 compteur = 0;
                 if (joueuractif == 1) {
-                    J1++;
+                    (*J1)++;
+                    printf("J1 : %d\n", *J1);
                 } else if (joueuractif == 2) {
-                    J2++;
+                    (*J2)++;
+                    printf("J2 : %d\n", *J2);
                 }
             }
         }
     }
 }
 
-void lancerLeDecompte(int cvert, int cblanc) {
+void lancerLeDecompte(int cnoir, int cblanc) {
 
-    rectfill(screen, 0, 0, 800, 600, cvert);
-    textprintf_ex(screen, font, 300, 300, cblanc, -1, "le jeu va commencez dans 3 ");
+    rectfill(screen, 200, 40, 610, 60, cnoir);
+    textprintf_ex(screen, font, 300, 40, cblanc, -1, "le jeu va commencez dans 3 ");
     rest(1000);
-    rectfill(screen, 300, 300, 510, 320, cvert);
-    textprintf_ex(screen, font, 300, 300, cblanc, -1, "le jeu va commencez dans 2 ");
+    rectfill(screen, 200, 40, 610, 60, cnoir);
+    textprintf_ex(screen, font, 300, 40, cblanc, -1, "le jeu va commencez dans 2 ");
     rest(1000);
-    rectfill(screen, 300, 300, 510, 320, cvert);
-    textprintf_ex(screen, font, 300, 300, cblanc, -1, "le jeu va commencez dans 1 ");
+    rectfill(screen, 300, 40, 510, 60, cnoir);
+    textprintf_ex(screen, font, 300, 40, cblanc, -1, "le jeu va commencez dans 1 ");
     rest(1000);
-    rectfill(screen, 300, 300, 510, 320, cvert);
-    textprintf_ex(screen, font, 380, 300, cblanc, -1, "START ! ");
+    rectfill(screen, 200, 40, 610, 60, cnoir);
+    textprintf_ex(screen, font, 380, 40, cblanc, -1, "START ! ");
     rest(1000);
-    rectfill(screen, 300, 300, 510, 320, cvert);
+    rectfill(screen, 200, 40, 610, 60, cnoir);
 
 }
 
-void afficherScores(int* J1, int* J2, int cbleu, int crouge, int cblanc) {
+void afficherScores(int* J1, int* J2, int cbleu, int crouge, int cblanc, int cnoir) {
+    rectfill(screen, 0, 0, 800, 72, cblanc);
+    rectfill(screen, 2, 2, 797, 70, cnoir);
     rectfill(screen, 8, 10, 110, 20, cbleu);
-    textprintf_ex(screen, font, 10, 10, cblanc, -1, "Joueur 1 : %d",  J1);
+    textprintf_ex(screen, font, 10, 10, cblanc, -1, "Joueur 1 : %d",  *J1);
     rectfill(screen, 8, 20, 110, 30, crouge);
-    textprintf_ex(screen, font, 10, 20, cblanc, -1, "Joueur 2 : %d",  J2);
+    textprintf_ex(screen, font, 10, 20, cblanc, -1, "Joueur 2 : %d",  *J2);
 }
 
 int verifierscores(int* J1, int* J2, int cblanc){
-    if (J1 > J2) {
-        textprintf_ex(screen, font, 300, 300, cblanc, -1, "Joueur 1 a gagné avec %d points", J1);
-        textprintf_ex(screen, font, 300, 300, cblanc, -1, "Joueur 2 a perdu avec %d points", J2);
+    if (*J1 > *J2) {
+        textprintf_ex(screen, font, 300, 40, cblanc, -1, "Joueur 1 a gagné avec %d points", *J1);
+        textprintf_ex(screen, font, 300, 40, cblanc, -1, "Joueur 2 a perdu avec %d points", *J2);
         return 1;
-    } else if (J2 > J1) {
-        textprintf_ex(screen, font, 300, 300, cblanc, -1, "Joueur 2 a gagné avec %d points", J2);
-        textprintf_ex(screen, font, 300, 300, cblanc, -1, "Joueur 1 a perdu avec %d points", J1);
+    } else if (*J2 > *J1) {
+        textprintf_ex(screen, font, 300, 40, cblanc, -1, "Joueur 2 a gagné avec %d points", *J2);
+        textprintf_ex(screen, font, 300, 40, cblanc, -1, "Joueur 1 a perdu avec %d points", *J1);
         return 2;
     } else {
-        textprintf_ex(screen, font, 250, 300, cblanc, -1, "Egalité ! Les deux joueurs ont %d points", J1);
+        textprintf_ex(screen, font, 250, 40, cblanc, -1, "Egalité ! Les deux joueurs ont %d points", *J1);
         return 3;
     }
 }
@@ -70,6 +76,7 @@ int main(int argc, char *argv[]) {
     allegro_init();
     install_keyboard();
     install_mouse();
+    install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,NULL);
 
     set_color_depth(desktop_color_depth());
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0) != 0) {
@@ -82,17 +89,20 @@ int main(int argc, char *argv[]) {
     BITMAP *sprite1 = load_bitmap("../Projet_2/image_taupe0.bmp", NULL);
     BITMAP *sprite2 = load_bitmap("../Projet_2/image_taupe1.bmp", NULL);
     BITMAP *sprite3 = load_bitmap("../Projet_2/image_taupe2.bmp", NULL);
+    BITMAP *sprite4 = load_bitmap("../Projet_2/fuite_fumée.bmp", NULL);
 
 
     BITMAP *dessprite1 = create_bitmap(150, 150);
     BITMAP *dessprite2 = create_bitmap(150, 150);
     BITMAP *dessprite3 = create_bitmap(150, 150);
+    BITMAP *dessprite4 = create_bitmap(150, 150);
 
     if (!sprite1 || !sprite2 || !sprite3) {
         sprite1 = load_bitmap("Projet_2\\image_taupe0.bmp", NULL);
         sprite2 = load_bitmap("Projet_2\\image_taupe1.bmp", NULL);
         sprite3 = load_bitmap("Projet_2\\image_taupe2.bmp", NULL);
-        if (!sprite1 || !sprite2 || !sprite3) {
+        sprite4 = load_bitmap("Projet_2\\fuite_fumée.bmp", NULL);
+        if (!sprite1 || !sprite2 || !sprite3 || !sprite4) {
             allegro_message("prb gfx mode");
             allegro_exit();
             exit(EXIT_FAILURE);
@@ -102,45 +112,48 @@ int main(int argc, char *argv[]) {
     stretch_blit(sprite1, dessprite1, 0, 0, sprite1->w, sprite1->h, 0, 0, dessprite1->w, dessprite1->h);
     stretch_blit(sprite2, dessprite2, 0, 0, sprite2->w, sprite2->h, 0, 0, dessprite2->w, dessprite2->h);
     stretch_blit(sprite3, dessprite3, 0, 0, sprite3->w, sprite3->h, 0, 0, dessprite3->w, dessprite3->h);
+    stretch_blit(sprite4, dessprite4, 0, 0, sprite4->w, sprite4->h, 0, 0, dessprite4->w, dessprite4->h);
+    
+    SAMPLE *son1 = load_sample("../Projet_2/son/son1.wav");// ajouter fond sonore
+    SAMPLE *son2 = load_sample("../Projet_2/son/son2.wav");// son animation et appuie du bouton
+    SAMPLE *son3 = load_sample("../Projet_2/son/son3.wav");// quand une image sort
+    SAMPLE *son4 = load_sample("../Projet_2/son/son4.wav");// quand on gagne
 
-    int cvert = makecol(34, 177, 76) , cbleu = makecol(0, 0, 255),cblanc = makecol(255, 255, 255), crouge = makecol(255, 0, 0),nbrdetours = 0,joueuractif = 1;
+    play_sample( son1, 255, 128, 1000, 1);// jouer le son 1 (fond sonore)
+
+    int cvert = makecol(34, 177, 76) , cbleu = makecol(0, 0, 255),cblanc = makecol(255, 255, 255),cnoir = makecol(0,0,0), crouge = makecol(255, 0, 0),nbrdetours = 0,joueuractif = 1;
     int gagnant = 0;
-    int* J1 = 0;
-    int* J2 = 0;
+    int J1 = 0;
+    int J2 = 0;
+
+    int *pJ1 = &J1;
+    int *pJ2 = &J2;
 
     rectfill(screen, 0, 0, 800, 600, cvert);
+    afficherScores(pJ1, pJ2, cbleu, crouge, cblanc, cnoir);
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             draw_sprite(screen, dessprite1 ,120 + i * 200, 80 + j * 150);
         }
     }
-    textprintf_ex(screen, font, 290, 300, cblanc, -1, "Le joueur 1 ce prépare pour jouer");
+    textprintf_ex(screen, font, 270, 10, cblanc, -1, "BIENVENUE DANS LE JEU TAPE TAUPE");
+    textprintf_ex(screen, font, 270, 40, cblanc, -1, "Le joueur 1 ce prépare pour jouer");
     rest(5000);
-    lancerLeDecompte(cvert, cblanc);
+    lancerLeDecompte(cnoir, cblanc);
 
     while(joueuractif != 3){
 
         int rectbleu = rand() % 12 + 1;
-        int tempsderepos = rand() % 500 + 500;
-        int compteur = rand () % 6000 + 4000;
+        int tempsderepos = rand() % 500 + 400;
+        int compteur = rand () % 1000 + 1000;
 
         //création des rectangles verts
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 draw_sprite(screen, dessprite1 ,120 + i * 200, 80 + j * 150);
             }
         }
-        //verification du nbr de tours pour changer de joueur
-        if (nbrdetours == 15) {
-            joueuractif = 2;
-            textprintf_ex(screen, font, 310, 300, cblanc, -1, "Joueur 1 a %d points", J1);
-            textprintf_ex(screen, font, 270, 270, cblanc, -1, "Joueur 2 ce prépare pour jouer", J2);
-            rest(5000);
-            lancerLeDecompte(cvert, cblanc);
-        } else if (nbrdetours == 30) {
-            joueuractif = 3;
-        }
-
         // temps de repos entre chaque rectangle bleu entre 500 et 1000 ms
         rest (tempsderepos);
 
@@ -148,43 +161,52 @@ int main(int argc, char *argv[]) {
         nbrdetours++;
         printf("nbr de tours : %d\n", nbrdetours);
 
-        //affichage des scores
-        afficherScores(J1, J2, cbleu, crouge, cblanc);
-
         //affichage des rectangles bleus (taupe qui sortent)
         if (rectbleu == 1) {
             printf("rectbleu vaut 1\n");
-            dessinerRectangleBleu(120, 80, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(120, 80, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 2) {
             printf("rectbleu vaut 2\n");
-            dessinerRectangleBleu(320, 80, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(320, 80, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 3) {
             printf("rectbleu vaut 3\n");
-            dessinerRectangleBleu(520, 80, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(520, 80, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 4) {
             printf("rectbleu vaut 4\n");
-            dessinerRectangleBleu(120, 230, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(120, 230, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 5) {
             printf("rectbleu vaut 5\n");
-            dessinerRectangleBleu(320, 230, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(320, 230, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 6) {
             printf("rectbleu vaut 6\n");
-            dessinerRectangleBleu(520, 230, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(520, 230, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 7) {
             printf("rectbleu vaut 7\n");
-            dessinerRectangleBleu(120, 380, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(120, 380, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 8) {
             printf("rectbleu vaut 8\n");
-            dessinerRectangleBleu(320, 380, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(320, 380, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
         }else if (rectbleu == 9) {
             printf("rectbleu vaut 9\n");
-            dessinerRectangleBleu(520, 380, cbleu, crouge, compteur, joueuractif, J1, J2, dessprite2, dessprite3);
+            dessinerRectangleBleu(520, 380, cbleu, crouge, compteur, joueuractif, pJ1, pJ2, dessprite2, dessprite3, dessprite4);
 
         }
+        //verification du nbr de tours pour changer de joueur
+        if (nbrdetours == 15) {
+            joueuractif = 2;
+            textprintf_ex(screen, font, 310, 40, cblanc, -1, "Joueur 1 a %d points", pJ1);
+            textprintf_ex(screen, font, 270, 60, cblanc, -1, "Joueur 2 ce prépare pour jouer", pJ2);
+            rest(5000);
+            lancerLeDecompte(cnoir, cblanc);
+        } else if (nbrdetours == 30) {
+            joueuractif = 3;
+        }
+        //affichage des scores
+        afficherScores(pJ1, pJ2, cbleu, crouge, cblanc,cnoir);
     }
 
     //affichage des scores finaux
-    gagnant = verifierscores(J1, J2, cblanc);
+    gagnant = verifierscores(pJ1, pJ2, cblanc);
     readkey();
     allegro_exit();
     EXIT_SUCCESS;
