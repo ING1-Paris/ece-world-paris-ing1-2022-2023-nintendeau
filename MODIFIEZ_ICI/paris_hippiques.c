eaf33934ec06c9ade25f88b5a2085741c282655f
shaïma <114345002+shm0m@users.noreply.github.com>
2023-05-18 00:49:31 +0200

    jeu officiellement terminé

--- MODIFIEZ_ICI/paris_hippiques.c
+++ MODIFIEZ_ICI/paris_hippiques.c
@@ -12,11 +12,14 @@ int main() {
     allegro_init();
     install_keyboard();
     set_color_depth(32);
+    install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL);
+    SAMPLE *music = load_wav("../son/accueil.wav");
+    play_sample(music, 255, 128, 1000, 1); //permet de jouer de la musique en boucle
 
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, SCREEN_W, SCREEN_H, 0 , 0);
 
-    BITMAP *accueil = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\accueil.bmp",NULL); //création d'un bitmap
-    BITMAP *choixjoueur1 = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\choixjoueur1.bmp",NULL);
+    BITMAP *accueil = load_bitmap("../images/accueil.bmp",NULL); //création d'un bitmap
+    BITMAP *choixjoueur1 = load_bitmap("../images/choixjoueur1.bmp",NULL);
 
     while (!key[KEY_ENTER]) {
         blit(accueil, screen, 0, 0, 0, 0, 1500, 700); //affichage de l'image
@@ -27,22 +30,31 @@ int main() {
     if (key[KEY_ENTER]) {
         clear(accueil);
         destroy_bitmap(accueil);
-        BITMAP *choixjoueur1 = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\choixjoueur1.bmp",NULL);
+        BITMAP *choixjoueur1 = load_bitmap("../images/choixjoueur1.bmp",NULL);
         blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
     }
+
 
     while (choix1 == 0) {
         if (key[KEY_1]) {
             choix1 = 1;
+            textprintf_ex(choixjoueur1, font, 600, 620, makecol(0, 55, 255), -1, "JOUEUR 1,VOUS AVEZ CHOISI LE CHEVAL 1", choix1);
+            blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
         }
         if (key[KEY_2]) {
             choix1 = 2;
+            textprintf_ex(choixjoueur1, font, 600, 620, makecol(0, 55, 255), -1, "JOUEUR 1,VOUS AVEZ CHOISI LE CHEVAL 2", choix1);
+            blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
         }
         if (key[KEY_3]) {
             choix1 = 3;
+            textprintf_ex(choixjoueur1, font, 600, 620, makecol(0, 55, 255), -1, "JOUEUR 1,VOUS AVEZ CHOISI LE CHEVAL 3", choix1);
+            blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
         }
         if (key[KEY_4]) {
             choix1 = 4;
+            textprintf_ex(choixjoueur1, font, 600, 620, makecol(0, 55, 255), -1, "JOUEUR 1,VOUS AVEZ CHOISI LE CHEVAL 4", choix1);
+            blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
         }
         if (key[KEY_5]) {
             choix1 = 5;
@@ -51,12 +63,15 @@ int main() {
     if (choix1 == 5) {
         allegro_exit();
     }
+
     rest(2000);
     clear_bitmap(choixjoueur1);
     destroy_bitmap(choixjoueur1);
 
-    BITMAP *choixjoueur2 = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\choixjoueur2.bmp",NULL);
+    BITMAP *choixjoueur2 = load_bitmap("../images/choixjoueur2.bmp",NULL);
     blit(choixjoueur2, screen, 0, 0, 0, 0, 1500, 700);
+
+    if (choixjoueur2)
     while (choix2 == 0) {
         if (key[KEY_1]) {
             choix2 = 1;
@@ -79,8 +94,8 @@ int main() {
         }
 
     if (choix1== choix2){
-        textprintf_ex(choixjoueur2, font, 10, 10, makecol(255, 255, 255), -1, "VOUS AVEZ CHOISI LE MEME CHEVAL");
-        textprintf_ex(choixjoueur2, font, 10, 30, makecol(255, 255, 255), -1, "Appuyez sur ENTREE pour recommencer.");
+        textprintf_ex(choixjoueur2, font, 600, 620, makecol(255, 0, 0), -1, "VOUS AVEZ CHOISI LE MEME CHEVAL");
+        textprintf_ex(choixjoueur2, font, 600, 650, makecol(255, 0, 0), -1, "Appuyez sur ENTREE pour recommencer.");
         blit(choixjoueur2, screen, 0, 0, 0, 0, 1500, 700);
         while (!key[KEY_ENTER]) {
             blit(choixjoueur2, screen, 0, 0, 0, 0, 1500, 700);
@@ -98,7 +113,9 @@ int main() {
         blit(choixjoueur2, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
     }
     clear(choixjoueur2);
-    blit(choixjoueur2, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+    destroy_bitmap(choixjoueur2);
+    stop_sample(music); //arret de la musique d'accueil
+
 
     if (key[KEY_ENTER]){
 
@@ -152,24 +169,36 @@ int main() {
             exit(EXIT_FAILURE);
         }
         //initialisation des variables de position et de vitesse pour chaque sprite
-        float x1 = 99, y1 = 580, speed1 = rand() % 20 + 1;
-        float x2 = 99, y2 = 550, speed2 = rand() % 20 + 1;
-        float x3 = 99, y3 = 520, speed3 = rand() % 20 + 1;
-        float x4 = 99, y4 = 490, speed4 = rand() % 20 + 1;
+        float x1 = 88, y1 = 580, speed1 = 1 + rand() % 10 + 1;
+        float x2 = 88, y2 = 550, speed2 = 1 + rand() % 10 + 1;
+        float x3 = 88, y3 = 520, speed3 = 1 + rand() % 10 + 1;
+        float x4 = 88, y4 = 490, speed4 = 1 + rand() % 10 + 1;
 
-        textprintf_ex(background, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "3");
-        blit(background, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-        rest(1000);
-        textprintf_ex(background, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "2");
-        blit(background, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-        rest(1000);
-        textprintf_ex(background, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "1");
-        blit(background, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-        rest(1000);
-        textprintf_ex(background, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "GO !");
-        blit(background, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-        rest(1000);
+        SAMPLE *music2 = load_sample("../son/course1.wav"); //chargement de la musique de course
+        if (!music2) {
+            allegro_message("Erreur lors du chargement de la musique");
+            exit(EXIT_FAILURE);
+        }
+        play_sample(music2, 255, 128, 1000, 1); //lancement de la musique de course
 
+        textprintf_ex(page, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "3");
+        blit(page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+        rest(1000);
+        clear(page);
+        textprintf_ex(page, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "2");
+        blit(page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+        rest(1000);
+        clear(page);
+        textprintf_ex(page, font, SCREEN_W/2, SCREEN_H/2, makecol(255, 255, 255), -1, "1");
+        SAMPLE *music3 = load_sample("../son/pistolet.wav"); //chargement de la musique de course
+        if (!music3) {
+            allegro_message("Erreur lors du chargement de la musique");
+            exit(EXIT_FAILURE);
+        }
+        play_sample(music3, 255, 128, 1000, 0); //lancement de la musique de course
+        blit(page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+        rest(1000);
+        clear(page);
 
 
         //boucle de jeu principal
@@ -177,22 +206,23 @@ int main() {
             compteur++;
             if(compteur % 4==0){ //changer d'image toutes les 4 frames (pour ralentir l'animation)
                 //changer d'image
+                rest(50);
                 anim++;
                 if (anim > 2)
                     anim = 0;
             }
 
             if(speed4 == speed1){
-                speed1 = rand() % 20 + 1;
+                speed1 = 1 + rand() % 10 + 1;
             }
             if(speed3 == speed2){
-                speed3 = rand() % 20 + 1;
+                speed3 = 1 + rand() % 10 + 1;
             }
             if(speed2 == speed1){
-                speed2 = rand() % 20 + 1;
+                speed2 = 1 + rand() % 10 + 1;
             }
             if (speed2 == speed4){
-                speed4 = rand() % 20 + 1;
+                speed4 = 1 + rand() % 10 + 1;
             }
 
             //affichage du fond
@@ -202,7 +232,7 @@ int main() {
 
 
             //déplacement du cheval 1
-            x1 += speed1/10;
+            x1 += speed1/4;
             if (x1 > 1410) {
                 gagne = true;
                 if(choix1 == 1){
@@ -210,21 +240,24 @@ int main() {
                     soldeticket1 += 1;
                     soldeticket2 -= 1;
                     allegro_message("Joueur 1,remporte la course !");
+                    stop_sample(music2);
                 }else if(choix2 ==1){
                     score2 ++;
                     soldeticket2 += 1;
                     soldeticket1 -= 1;
                     allegro_message("Joueur 2,remporte la course !");
+                    stop_sample(music2);
                 }else{
                     soldeticket1 -= 1;
                     soldeticket2 -= 1;
                     allegro_message("Match nul ! Solde J1 = %d, Solde J2 = %d",soldeticket1,soldeticket2);
+                    stop_sample(music2);
                 }
             }
             draw_sprite(page, sprite1[anim], x1, y1);
 
             //déplacement du cheval 2
-            x2 += speed2/10;
+            x2 += speed2/4;
             if (x2 > 1410) {
                 gagne = true;
                 if (choix1 == 2) {
@@ -232,21 +265,24 @@ int main() {
                     soldeticket1 += 1;
                     soldeticket2 -= 1;
                     allegro_message("Joueur 1,remporte la course !");
+                    stop_sample(music2);
                 } else if (choix2 == 2) {
                     score2++;
                     soldeticket2 += 1;
                     soldeticket1 -= 1;
                     allegro_message("Joueur 2,remporte la course !");
+                    stop_sample(music2);
                 } else {
-                    soldeticket1 -= 1; //
+                    soldeticket1 -= 1;
                     soldeticket2 -= 1;
                     allegro_message("Match nul ! Solde J1 = %d, Solde J2 = %d", soldeticket1, soldeticket2);
+                    stop_sample(music2);
                 }
             }
             draw_sprite(page, sprite2[anim], x2, y2);
 
             //déplacement du cheval 3
-            x3 += speed3/10; //vitesse du cheval divisée par 10 pour ralentir le cheval
+            x3 += speed3/4; //vitesse du cheval divisée par 10 pour ralentir le cheval
             if (x3 > 1410) {
                 gagne = true;
                 if(choix1 == 3){
@@ -254,21 +290,24 @@ int main() {
                     soldeticket1 += 1;
                     soldeticket2 -= 1;
                     allegro_message("Joueur 1,remporte la course !");
+                    stop_sample(music2);
                 }else if(choix2 ==3){
                     score2 ++;
                     soldeticket2 += 1;
                     soldeticket1 -= 1;
                     allegro_message("Joueur 2,remporte la course !");
+                    stop_sample(music2);
                 } else{
                     soldeticket1 -= 1;
                     soldeticket2 -= 1;
                     allegro_message("Match nul ! Solde J1 = %d, Solde J2 = %d",soldeticket1,soldeticket2);
+                    stop_sample(music2);
                 }
             }
             draw_sprite(page, sprite3[anim], x3, y3);
 
             //deplacement du cheval 4
-            x4 += speed4/10;
+            x4 += speed4/4;
             if (x4 > 1410) {
                 gagne = true;
                 if(choix1 == 4){
@@ -276,20 +315,25 @@ int main() {
                     soldeticket1 += 1;
                     soldeticket2 -= 1;
                     allegro_message("Joueur 1,remporte la course !");
+                    stop_sample(music2);
                 }else if(choix2 ==4){
                     score2 ++;
                     soldeticket2 += 1;
                     soldeticket1 -= 1;
                     allegro_message("Joueur 2,remporte la course !");
+                    stop_sample(music2);
                 } else{
                     soldeticket1 -= 1;
                     soldeticket2 -= 1;
                     allegro_message("Match nul ! Solde J1 = %d, Solde J2 = %d",soldeticket1,soldeticket2);
+                    stop_sample(music2);
                 }
             }
             draw_sprite(page, sprite4[anim], x4, y4);
             blit(page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+
         }
+        stop_sample(music2);
 
     }
     return 0;
@@ -297,8 +341,8 @@ int main() {
 
 void choix(){
 
-    BITMAP *accueil = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\accueil.bmp",NULL); //création d'un bitmap
-    BITMAP *choixjoueur1 = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\choixjoueur1.bmp",NULL);
+    BITMAP *accueil = load_bitmap("../images/accueil.bmp",NULL); //création d'un bitmap
+    BITMAP *choixjoueur1 = load_bitmap("../images/choixjoueur1.bmp",NULL);
 
     while (!key[KEY_ENTER]) {
         blit(accueil, screen, 0, 0, 0, 0, 1500, 700); //affichage de l'image
@@ -309,7 +353,7 @@ void choix(){
     if (key[KEY_ENTER]) {
         clear(accueil);
         destroy_bitmap(accueil);
-        BITMAP *choixjoueur1 = load_bitmap("C:\\Users\\shaïma\\CLionProjects\\parishippiques\\images\\choixjoueur1.bmp",NULL);
+        BITMAP *choixjoueur1 = load_bitmap("../images/choixjoueur1.bmp",NULL);
         blit(choixjoueur1, screen, 0, 0, 0, 0, 1500, 700);
     }
 
